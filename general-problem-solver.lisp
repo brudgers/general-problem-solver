1276c7ec5b33f411947315e7631ae486ae6c4301
ben rudgers <ben.rudgers@gmail.com>
2017-01-31 17:27:16 -0600

    fixed bug in apply-op

--- general-problem-solver.lisp
+++ general-problem-solver.lisp
@@ -21,16 +21,16 @@
   (add-list nil)
   (del-list nil))
 
-(defun GPS (*state* goals *ops*)
+(defun gps (*state* goals *ops*)
   "General Problem Solver: achieve all goals using *ops*."
   (if (every #'achieve goals) 'solved))
 
 (defun achieve (goal)
   "A goal is achieved if it already holds. Or if there
    is an appropriate op for it that is applicable."
-  (or (member goal state)
+  (or (member goal *state*)
       (some #'apply-op
-            (find-all goal *ops* :test appropriate-p))))
+            (find-all goal *ops* :test #'appropriate-p))))
 
 (defun appropriate-p (goal op)
   "An op is appropriate to a goal if the goal is on the op's add-list."
@@ -41,8 +41,7 @@
   (when (every #'achieve (op-preconds op))
     (print (list 'executing (op-action op)))
     (setf *state* (set-difference *state* (op-del-list op)))
-    (setf *state* (union *state* (op-add-list op))))
-  t)
+    (setf *state* (union *state* (op-add-list op)))))
 
 (defparameter *school-ops*
   (list
